67bcbddf373615bdcd283c5b00d1d402eab4bdef
Lidia <lidia.luczkiewicz@pw.edu.pl>
2024-05-27 12:33:05 +0200

    config for visualization in rviz

---
+++ src/model/launch/model_launch.py
@@ -0,0 +1,44 @@
+from launch import LaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+import os
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    model = FindPackageShare(package="model").find("model")
+
+    # Get the paths for URDF model and RViz configuration
+    my_model_path = os.path.join(model, "urdf/model.urdf")
+    
+    robot_description_content = ParameterValue(
+        Command(["xacro ", my_model_path]), value_type=str
+    )
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": robot_description_content,
+            }
+        ],
+    )
+
+    ld.add_action(robot_state_publisher_node)
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen"
+    )
+
+    ld.add_action(rviz)
+
+    return ld
---
+++ src/model/setup.py
@@ -0,0 +1,29 @@
+from setuptools import find_packages, setup
+import os
+from glob import glob
+package_name = 'model'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*')))
+
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='lidka',
+    maintainer_email='lidia.luczkiewicz@pw.edu.pl',
+    description='NURT robot model',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
