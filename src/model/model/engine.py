22b8cb505c575ee662030209181addfc663c561e
Lidia <lidia.luczkiewicz@pw.edu.pl>
2024-05-28 15:19:53 +0200

    working but needs refactor

--- src/model/model/engine.py
+++ src/model/model/engine.py
@@ -29,6 +29,7 @@ class Engine(Node):
 
         self.init_joints()
         self.iter = 0
+        self.state = 0
         self.timer = self.create_timer(0.01, self.update_joints)
 
     def init_joints(self):
@@ -67,11 +68,18 @@ class Engine(Node):
                 # reached target tolerance  
                 self.jointMsg.position = self.calculate_reverse_kinematic(goal_position[0], goal_position[1])
                 self.iter = 0
+                self.state = 0     
             else:
                 target_x = float(current_position[0] + (difference_vector[0]) / max(2, (1000 - self.iter)))
                 target_y = float(current_position[1] + (difference_vector[1]) / max(2, (1000 - self.iter)))
                 self.iter += 1
-                self.jointMsg.position = self.calculate_reverse_kinematic(target_x, target_y)
+                position = self.calculate_reverse_kinematic(target_x, target_y)
+                if self.state == 0:
+                    self.jointMsg.position[0] = position[0]
+                    if abs(self.jointMsg.position[0] - self.calculate_reverse_kinematic(goal_position[0], goal_position[1])[0]) < 0.0001:
+                        self.state = 1
+                if self.state == 1:
+                    self.jointMsg.position = position
 
     def calculate_reverse_kinematic(self, x0, y0):
             theta1 = atan2(y0, x0)
