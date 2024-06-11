23e0d8e2cffb8a23b24215084ba6e6ce7664c6d0
Lidia <lidia.luczkiewicz@pw.edu.pl>
2024-06-11 18:18:42 +0200

    model with more realistic parts working on gui

---
+++ src/model/launch/fusion_model_launch.py
@@ -0,0 +1,53 @@
+from ament_index_python import get_package_share_directory
+from launch import LaunchDescription
+from launch.substitutions import Command, LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.parameter_descriptions import ParameterValue
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition
+import os, xacro
+
+def generate_launch_description():
+    ld = LaunchDescription()
+
+    share_dir = get_package_share_directory('model')
+
+    # Get robot description
+
+    xacro_file = os.path.join(share_dir, 'urdf', 'fusion_model.xacro')
+
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_description_content = robot_description_config.toxml()
+
+    robot_state_publisher_node = Node(
+        package="robot_state_publisher",
+        executable="robot_state_publisher",
+        output="screen",
+        parameters=[
+            {
+                "robot_description": robot_description_content,
+            }
+        ],
+    )
+
+    ld.add_action(robot_state_publisher_node)
+
+    rviz = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="screen"
+    )
+
+    ld.add_action(rviz)
+
+    joint_state_publisher_gui_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui'
+    )
+
+    ld.add_action(joint_state_publisher_gui_node)
+
+    return ld
--- src/model/setup.py
+++ src/model/setup.py
@@ -12,8 +12,8 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
         (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*'))),
-        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*')))
-
+        (os.path.join('share', package_name, 'urdf'), glob(os.path.join('urdf', '*'))),
+        (os.path.join('share', package_name, 'meshes'), glob(os.path.join('meshes', '*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
